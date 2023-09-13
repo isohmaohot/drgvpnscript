359e2a4ace71b919fdf55594dae2d627d09fbe81
isohmaohot <sarahlaurel713@gmail.com>
2023-09-13 08:02:25 +0700

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -9,17 +9,17 @@ make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/backupBot.sh"
+wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/autobackup.sh"
 wget -O backup "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/backup.sh"
 wget -O bckp "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/bckp.sh"
 wget -O restore "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/strt.sh"
+wget -O running "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/running.sh"
 wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
 chmod +x restore
-chmod +x strt
+chmod +x running
 chmod +x limit-speed
 chmod +x clear-log
 cd

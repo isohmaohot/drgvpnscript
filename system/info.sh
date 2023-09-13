27819dd25a05f9f0c56f6ca6663bfd6c152fb470
isohmaohot <sarahlaurel713@gmail.com>
2023-09-13 09:26:05 +0700

    Update info.sh

--- system/info.sh
+++ system/info.sh
@@ -17,13 +17,13 @@ echo ""
 figlet " SCRIPT" | lolcat
 figlet " BY drgvpn" | lolcat
 echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
-echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
+echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS          \e[0m  \e[32;1m|\e[0m"
 echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
-echo -e "           \e[031;1m> Premium Script By Wednesday <\e[0m" | lolcat
-echo -e " \e[0;32m_______________________________________________\e[0m"
+echo -e "\e[031;1m>            Premium Script By KyyLearning     <\e[0m" | lolcat
+echo -e "\e[0;32m'_______________________________________________'\e[0m"
 echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
-echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare drgvpn         \e[0;32m|\e[0m"
+echo -e "\e[0;32m|    \e[1;35mFor VPS with KVM and VMWare drgvpn         \e[0;32m|\e[0m"
 echo -e "\e[0;32m|               \e[1;35mI HOPE YOU HAPPY                \e[0;32m|\e[0m"
-echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
+echo -e "\e[0;32m'_______________________________________________'\e[0;32m|\e[0m"
 echo -e "\e[0;32m|_______________\e[36;1mTHANKYOU SUPPORT\e[0m\e[0;32m________________|\e[0m"
-echo -e "               \e[0;32m'----------------'\e[0m"
+echo -e "                        \e[0;32m'----------------'\e[0m"
